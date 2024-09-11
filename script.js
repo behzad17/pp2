117ee5dcc59fec1fa90809808482d9a65e41861a
Behzad Javadian <behzad_1349@hotmail.com>
2024-09-11 22:33:36 +0000

    add variabales to questions in js file

---
+++ script.js
@@ -0,0 +1,45 @@
+const question = [ 
+    {
+        question: "how many seats are there in Sweden's parliament";
+        answers: [
+            {text: "347", correct:true};
+            {text: "348", correct:true};
+            {text: "349", correct:true};
+            {text: "350", correct:true};
+        
+         ]
+    };
+
+    {
+        question: "how many seats are there in Sweden's parliament";
+        answers: [
+            {text: "347", correct:true};
+            {text: "348", correct:true};
+            {text: "349", correct:true};
+            {text: "350", correct:true};
+        
+         ]
+    };
+
+    {
+        question: "how many seats are there in Sweden's parliament";
+        answers: [
+            {text: "347", correct:true};
+            {text: "348", correct:true};
+            {text: "349", correct:true};
+            {text: "350", correct:true};
+        
+         ]
+    };
+
+    {
+        question: "how many seats are there in Sweden's parliament";
+        answers: [
+            {text: "347", correct:true};
+            {text: "348", correct:true};
+            {text: "349", correct:true};
+            {text: "350", correct:true};
+        
+         ]
+    };
+]
