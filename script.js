58c5e107ec8c6630c75a97f1df652998bfafbf48
Behzad Javadian <behzad_1349@hotmail.com>
2024-09-11 22:53:04 +0000

    add 3 more const variables to js file and adjust the correct answers

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 const question = [ 
     {
-        question: "how many seats are there in Sweden's parliament";
+        question: "how many seats are there in Sweden's parliament?",
         answers: [
-            {text: "347", correct:true};
-            {text: "348", correct:true};
-            {text: "349", correct:true};
-            {text: "350", correct:true};
+            {text: "347", correct:false},
+            {text: "348", correct:false},
+            {text: "349", correct:true},
+            {text: "350", correct:false},
         
          ]
     };
 
     {
-        question: "how many seats are there in Sweden's parliament";
+        question: "how many partys is there in parlament 2024?",
         answers: [
-            {text: "347", correct:true};
-            {text: "348", correct:true};
-            {text: "349", correct:true};
-            {text: "350", correct:true};
+            {text: "8", correct:true},
+            {text: "9", correct:false},
+            {text: "11", correct:false},
+            {text: "12", correct:false},
         
          ]
     };
 
     {
-        question: "how many seats are there in Sweden's parliament";
+        question: "What is the bigest party in Sweden's parlament?";
         answers: [
-            {text: "347", correct:true};
-            {text: "348", correct:true};
-            {text: "349", correct:true};
-            {text: "350", correct:true};
+            
+            {text: "The Moderate Party", correct:false},
+            {text: "The Left Party", correct:false},
+            {text: "The Sweden Democrats", correct:false},
+            {text: "The Social Democratic Party", correct:true},
         
          ]
     };
 
     {
-        question: "how many seats are there in Sweden's parliament";
+        question: "What is the smalest party in Sweden's parlament?",
         answers: [
-            {text: "347", correct:true};
-            {text: "348", correct:true};
-            {text: "349", correct:true};
-            {text: "350", correct:true};
+            {text: "The Centre Party", correct:false},
+            {text: "The Liberal Party", correct:true},
+            {text: "The Green Party", correct:false},
+            {text: "The Christian Democrats", correct:false},
         
          ]
-    };
-]
+    },
+];
