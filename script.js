d8e7ad46b27967c758a037453237fcbe686957ee
Behzad Javadian <behzad_1349@hotmail.com>
2024-09-12 00:57:11 +0000

    find some problems with starting the quiz

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const question = [
             {text: "350", correct:false},
         
          ]
-    };
+    },
 
     {
         question: "how many partys is there in parlament 2024?",
@@ -19,10 +19,10 @@ const question = [
             {text: "12", correct:false},
         
          ]
-    };
+        },
 
     {
-        question: "What is the bigest party in Sweden's parlament?";
+        question: "What is the bigest party in Sweden's parlament?",
         answers: [
             
             {text: "The Moderate Party", correct:false},
@@ -31,7 +31,7 @@ const question = [
             {text: "The Social Democratic Party", correct:true},
         
          ]
-    };
+        },
 
     {
         question: "What is the smalest party in Sweden's parlament?",
@@ -42,43 +42,60 @@ const question = [
             {text: "The Christian Democrats", correct:false},
         
          ]
-    },
+        },
 ];
 
-const questionElements = document.getElementById("question");
+const questionElement = document.getElementById("question");
 const answerbtns = document.getElementById("answerbtn");
-const nextbtn = document.getElementById("nextbtn");
+const nextBtn = document.getElementById("nextbtn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz (){
+function startQuiz(){
     currentQuestionIndex = 0;
-    score =0;
+    score = 0;
     nextBtn.innerHTML = "Next";
     showQuestion();
 }
+
 function showQuestion(){
     resetState();
     let currentQuestion = guestions[currentQuestionIndex];
     let questionNum = currentQuestionIndex + 1;
-    guestionElement.innerHTML = guestionNum + "." + currentQuestion.guestion;
+    guestionElement.innerHTML = guestionNum + "." + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
         const btn = document.createElement("button");
-        btn.innerHTML = answer.text;
+        btn.innerHTML = answer.correct;
         btn.classList.add("btn");
-        answerbtns.appendChild("btn");
+        answerBtns.appendChild("btn");
+
         if( answer.correct){
             btn.dataset.correct = answer.text;
         }
         btn.addEventListener('click', selectAnswer);
 
     });
-    }
+
+}
     function resetState(){
         nextBtn.style.display = "none";
         while(answerBtns.firstChild){
-            answerBtns.removeChild(answerBtns.firstChild)
+            answerBtns.removeChild(answerBtns.firstChild);
         }
+}
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
     }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    
+}
+
+startQuiz();
